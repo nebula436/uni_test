9f430373f544556e4b78041eda34e74fdfd6075d
DD <daniildatsyuk2000@gmail.com>
2025-11-30 18:21:52 +0200

    test

---
+++ лр2/лр2/лр2.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    cout << setw(12) << showpos << 4.0 << noshowpos << endl;
+    cout << setw(17) << fixed << setprecision(2) << internal << -67.09124 << endl;
+    cout << setw(10) << 235 << noshowpos << endl;
+
+}
